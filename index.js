9773865955bd087127fecd925d417399a7fec9d1
Michael Sunday <olawuni.michael@gmail.com>
2020-04-23 16:43:24 +0100

    Still on first commit o

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 //This is just a test file for github class with Mr. Fikayo
 
-const tiidelabHoH = {
+const tiidelabHoH = [
 	{
 		name: "Awesome Ojumu"
 	},
 	{
 		name: "Kemi Christiana Awoyele"
-	},
+	}
+]
 
-}
+tiidelabHoH.forEach(user=>{
+	console.log(user.name)
+})
